a4e9d9990b9925546fc3d829ec233812363315af
Albert <albertzeap@gmail.com>
2023-08-25 17:05:41 -0700

    Created supabaseConfig

--- src/apis/supabaseApi.js
+++ src/apis/supabaseApi.js
@@ -1,2 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
-export const supabase = createClient("https://tokvcdpslacxynzxammv.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRva3ZjZHBzbGFjeHluenhhbW12Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTI5OTE1MDgsImV4cCI6MjAwODU2NzUwOH0.AzLyjN1JMNeGMvs2JQQzO8ZDhZiHWX953wfTQCwIeKQ");
+
+
+
+export const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
