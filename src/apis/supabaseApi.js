ab6fc7613698607782e6912fa875620dd6496081
Albert <albertzeap@gmail.com>
2023-08-25 17:11:16 -0700

    Created development build option

--- src/apis/supabaseApi.js
+++ src/apis/supabaseApi.js
@@ -1,5 +1,14 @@
 import { createClient } from "@supabase/supabase-js";
+import { SUPABASE_URL } from "./supabaseConfig";
+import { SUPABASE_KEY } from "./supabaseConfig";
+
+export let supabase = 0;
+
+if (!process.env.SUPABASE_KEY || !process.env.SUPABASE_URL) {
+    supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+} else {
+
+    supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
+}
 
 
-
-export const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
